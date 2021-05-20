8541a639409f8b282d6343624d85b73c0044934e
risharipov <r.sharipov@cleverdata.ru>
2021-05-20 09:27:22 +0300

    access ec2 ami using data source

---
+++ lab_9/aws_data_sources.tf
@@ -0,0 +1,30 @@
+terraform {
+  # provider version is required for production use
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+data "aws_ami" "app_ami" {
+  most_recent = true
+
+  ## in case when you would like to use your own image use self instead
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "myec2" {
+  ami           = data.aws_ami.app_ami.image_id
+  instance_type = "t2.nano"
+}
